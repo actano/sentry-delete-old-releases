b8b5f39b3a3f0a9cfb8901fba25ee58a66347411
Fabian Frank <fabian.frank@actano.de>
2018-08-10 15:45:50 +0200

    Add catch block for rejection

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const { deleteReleasesOlderThanDays } = require('./lib/sentry-delete-old-releases')
 
 deleteReleasesOlderThanDays()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
