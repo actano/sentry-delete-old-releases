be8649d1ba8eada79999d88cfacd70ced9bd3221
Fabian Frank <fabian.frank@actano.de>
2018-08-13 11:51:08 +0200

    Add option for dry run

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { deleteReleasesOlderThanDays } = require('./lib/sentry-delete-old-releases')
 
-deleteReleasesOlderThanDays()
+const dryRun = !!process.argv[2]
+
+deleteReleasesOlderThanDays(dryRun)
   .catch((e) => {
     console.error(e)
     process.exit(1)
